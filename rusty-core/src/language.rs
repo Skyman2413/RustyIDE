9cfa6c9849336a6330619aca71fe22155581b867
Stepan <stepan26062001@gmail.com>
2025-03-06 02:43:54 +0300

    refactor

--- rusty-core/src/language.rs
+++ rusty-core/src/language.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait LanguageEngine: Send + Sync {
     async fn get_syntax_highlighting(&self, code: &str) -> Vec<()>;
-    async fn get_completions(&self, code: &str, position: usize) -> Vec<()>;
+    async fn get_completions(&self, code: &str, position: usize) -> Vec<String>;
 }
 
 pub struct MockEngine;
@@ -17,5 +17,11 @@ impl MockEngine {
 #[async_trait]
 impl LanguageEngine for MockEngine {
     async fn get_syntax_highlighting(&self, _code: &str) -> Vec<()> { vec![] }
-    async fn get_completions(&self, _code: &str, _position: usize) -> Vec<()> { vec![] }
+    async fn get_completions(&self, code: &str, _position: usize) -> Vec<String> {
+        if code.starts_with("pr") {
+            vec!["print".to_string(), "process".to_string()]
+        } else {
+            vec!["Это тест, б!?ть".to_string()]
+        }
+    }
 }
